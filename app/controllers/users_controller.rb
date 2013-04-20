7798056e077d12e6710c72d0be9657b7a48ef93f
Elliott Golden <studio@simplecircle.net>
2013-04-20 14:49:14 -0400

    refactored users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,27 +2,25 @@ class UsersController < ApplicationController
 
   layout :choose_layout
   before_filter :restrict_access, only:[:index, :show]
+  before_filter :find_resource, only: [:new, :create]
+
 
   def index
   end
 
   def new
-    @company = Company.find_by_subdomain!(request.subdomain)
     @user = User.new
     @profile = @user.build_profile
     @depts = CompanyDept.all
-    @tags = ActsAsTaggableOn::Tag.all.to_json(only: :name)
   end
 
   def create
-    @company = Company.find_by_subdomain!(request.subdomain)
     @user = User.create(params[:user])
-    @user.profile.skill_list = params[:skills]
     # Ensure this password doesn't already exist in future iterations before creation
     @user.password = SecureRandom.urlsafe_base64
-    @depts = params[:company_depts]
-    @tags = ActsAsTaggableOn::Tag.all.to_json(only: :name)
     @incoming_tags = params[:as_values_true]
+    @user.profile.skill_list = @incoming_tags.split(",").reject(&:empty?).join(",")
+    @depts = params[:company_depts]
     if @depts.nil?
       @user.errors.add(:categories, "You have to choose at least one category.")
       render "new"
@@ -45,6 +43,11 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def find_resource
+    @company = Company.find_by_subdomain!(request.subdomain)
+    @tags = ActsAsTaggableOn::Tag.all.to_json(only: :name)
+  end
 
   def choose_layout
     if ['new', 'confirmation'].include? action_name
