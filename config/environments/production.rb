35b5fbdf0845eac64144c3115c6fe45bae79422b
Elliott Golden <studio@simplecircle.net>
2013-04-23 13:35:30 -0400

    heavily edited prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,26 @@
 Innercircle::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+
+ config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.sendgrid.net",
+    :port => '25',
+    :domain => "getinnercircle.com",
+    :authentication => :plain,
+    :user_name => "elliottg",
+    :password => "sendgridbot"
+  }
 
   # Code is not reloaded between requests
   config.cache_classes = true
+  config.lograge.enabled = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
+  config.serve_static_assets = true
+  config.assets.debug = true
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
@@ -20,8 +30,15 @@ Innercircle::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  # Defaults to nil and saved in location specified by config.assets.prefix
-  # config.assets.manifest = YOUR_PATH
+  # Defaults to Rails.root.join("public/assets")
+  # This prevents it from being pushed up to Cloudfiles with all the other things in public/assets
+  config.assets.manifest = Rails.root.join("public/")
+
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  # config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -33,20 +50,15 @@ Innercircle::Application.configure do
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
-  # Prepend all log lines with the following tags
-  # config.log_tags = [ :subdomain, :uuid ]
-
   # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://fbb10cc56329cb5ea057-4ca1418a7613371d048778718c3acae9.r81.cf1.rackcdn.com"
 
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -60,8 +72,4 @@ Innercircle::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
