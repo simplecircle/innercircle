d6b8a5a3f2bdf6f743b9329c6e34fedfd21550bc
Elliott Golden <studio@simplecircle.net>
2013-04-25 09:35:36 -0400

    all of bootstrap js bacck in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap/bootstrap-transition
-//= require twitter/bootstrap/bootstrap-dropdown
-//= require twitter/bootstrap/bootstrap-collapse
+//= require twitter/bootstrap
 //= require placeholder.min
 //= require_tree .
