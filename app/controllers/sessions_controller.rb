7812239c5845e800dc97a6b5f684d1b847e2d3df
Kenny Chan <kenny.r.chan@gmail.com>
2013-05-09 19:28:55 -0400

    rm current_company from companies controll and redirect to talent link upon signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,10 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       cookies.permanent[:auth_token] = {value:user.auth_token, domain: :all}
       # this session will not be needed once all company finds are scoped through the subdomain
-      session[:company_id] = user.companies.first.id
-      redirect_to talent_url, :notice => "Logged in!"
+      # session[:company_id] = user.companies.first.id
+      subdomain = user.companies.first.subdomain
+      subdomain += "." unless subdomain.empty?
+      redirect_to talent_url(:host => subdomain + request.domain)
     else
       flash.now.alert = "Invalid email or password"
       render "new"
@@ -18,7 +20,7 @@ class SessionsController < ApplicationController
   def destroy
     cookies.delete(:auth_token, domain: :all)
     # this session will not be needed once all company finds are scoped through the subdomain
-    session[:company_id] = nil
-    redirect_to root_url, notice: "Logged out."
+    # session[:company_id] = nil
+    redirect_to root_url(subdomain: false), notice: "Logged out."
   end
 end
