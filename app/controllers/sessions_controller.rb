f8d4803ad58c0775e90ca283830d2aaadac1a133
Kenny Chan <kenny.r.chan@gmail.com>
2013-05-10 11:28:16 -0400

    Clean up sessions controller for companies logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       cookies.permanent[:auth_token] = {value:user.auth_token, domain: :all}
-      # this session will not be needed once all company finds are scoped through the subdomain
-      # session[:company_id] = user.companies.first.id
-      subdomain = user.companies.first.subdomain
-      subdomain += "." unless subdomain.empty?
-      redirect_to talent_url(:host => subdomain + request.domain)
+      redirect_to talent_url(subdomain: user.companies.first.subdomain)
     else
       flash.now.alert = "Invalid email or password"
       render "new"
@@ -19,8 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     cookies.delete(:auth_token, domain: :all)
-    # this session will not be needed once all company finds are scoped through the subdomain
-    # session[:company_id] = nil
     redirect_to root_url(subdomain: false), notice: "Logged out."
   end
 end
