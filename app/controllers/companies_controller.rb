458be5468f922535ca039ba2901c38e3199ea8fa
Kenny Chan <kenny.r.chan@gmail.com>
2013-05-14 18:50:50 -0400

    Change way verticals saved in Company controller so that we can remove vertical associations when updating a company profile

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,24 +9,42 @@ class CompaniesController < ApplicationController
     @verticals = Vertical.all
   end
 
+  def update
+    @company = current_user.owned_companies.find(params[:id])
+    if @company
+      @company.update_attributes! params[:company]
+      save_verticals(params[:verticals], @company)
+    end
+    redirect_to dashboard_url, notice:"Profile Updated"
+  end
+
   def create
     @company = Company.new(params[:company])
     @company.subdomain = params[:company][:name].to_slug.normalize(:separator=>"").to_s
     @user = @company.users.first
     @profile = @user.profile
 
-    @verticals = params[:verticals]
-    if @verticals
-      @verticals.each do |v|
-        CompaniesVertical.create!(company_id: @company.id, vertical_id: v)
-      end
-    end
+    @verticals = params[:verticals].map{|v| v.to_i}
     if @company.save
       @user = @company.users.first
+      save_verticals(@verticals, @company) if @verticals
       cookies.permanent[:auth_token] = {value: @user.auth_token, domain: :all}
       redirect_to dashboard_url(subdomain: @company.subdomain)
     else
       render "new"
+    end
+  end
+
+  def save_verticals(verticals, company)
+    #Destroy vertical association if the user unchecked it in form
+    company.verticals.each do |v|
+      if !verticals.include? v.id.to_s
+        CompaniesVertical.where(:company_id => company.id, :vertical_id=> v.id).destroy_all
+      end
+    end
+    verticals.each do |v|
+      logger.info v
+      CompaniesVertical.create!(company_id: company.id, vertical_id: v)
     end
   end
 
