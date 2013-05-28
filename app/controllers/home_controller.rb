5c9fff7c97cec1cd49cd89f1e1357d2468df06e1
Kenny Chan <kenny.r.chan@gmail.com>
2013-05-28 18:22:31 -0400

    Make homepage 2 columns

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,16 @@ class HomeController < ApplicationController
       redirect_to dashboard_url(subdomain: current_user.companies.first.subdomain)
     else #not an immediate redirect (ie no src query string), so we should clear the redirect source
       session[:redirect_source] = nil
-      @companies = Company.order('updated_at DESC')
+      @companyrows = []
+      currentrow = []
+      Company.order('updated_at DESC').each do |co|
+        if currentrow.length > 1
+          @companyrows << currentrow
+          currentrow = []
+        end
+        currentrow << co
+      end
+      @companyrows << currentrow if currentrow.length > 0
     end
   end
 end
