f92bd73ee9cce3e75da91ef9680e1090a44dbea2
Elliott Golden <studio@simplecircle.net>
2013-06-05 14:23:50 -0400

    added Sidekiq integration to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 require 'bundler/capistrano'
 require 'capistrano/ext/multistage'
 require './config/boot'
+require 'sidekiq/capistrano'
 
 set :stages, %w(production staging)
 set :default_stage, "staging"
@@ -28,6 +29,14 @@ set :use_sudo, false
 # Pull branch and create a cache of the repo in the shared dir, for quicker future deploys.
 set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
+
+# Additional flags are not needed on the sidekiq call cause sidekiq/capistrano appends them as needed.
+set :sidekiq_cmd, "bundle exec sidekiq"
+set :sidekiqctl_cmd, "bundle exec sidekiqctl"
+set :sidekiq_timeout, 10
+set :sidekiq_role, :app
+set :sidekiq_pid, "#{current_path}/tmp/pids/sidekiq.pid"
+set :sidekiq_processes, 1
 
 after "deploy:update_code".to_sym do
   upload "config/database.yml.cap", "#{release_path}/config/database.yml", :mode => 0444
@@ -58,7 +67,8 @@ namespace :unicorn do
 
   desc "Start unicorn"
   task :start, :roles => :app do
-    run "cd #{current_path} && bundle exec unicorn_rails -E #{stage} -D -c #{current_path}/config/unicorn.rb"
+    run "cd #{current_path} && bundle exec unicorn_rails -E production -D -c #{current_path}/config/unicorn.rb"
+    # run "cd #{current_path} && bundle exec unicorn_rails -E #{stage} -D -c #{current_path}/config/unicorn.rb"
   end
 
   desc "Restart unicorn"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,12 @@
-# set :deploy_to, "/var/www/sites/#{application}"
+set :deploy_to, "/var/www/sites/#{application}"
+set :rails_env, "production"
+set :branch, "master"
 # set :rails_env, "staging"
 # set :branch, "staging"
-# set :unicorn_pid, "#{deploy_to}/current/tmp/pids/unicorn.pid"
+set :unicorn_pid, "#{deploy_to}/current/tmp/pids/unicorn.pid"
 
 # Server innercircle(production)
-# role :app, "108.166.93.70:2012"
-# role :web, "108.166.93.70:2012"
-# role :db,  "108.166.93.70:2012", :primary => true
+role :app, "108.166.93.213:2012"
+role :web, "108.166.93.213:2012"
+role :db,  "108.166.93.213:2012", :primary => true
 
