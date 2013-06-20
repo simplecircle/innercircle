e04810158e16f3f2b9648c2480a35a13f05bce6f
Elliott Golden <studio@simplecircle.net>
2013-06-20 13:45:30 -0400

    deploy env tweaks

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,12 +1,9 @@
 set :deploy_to, "/var/www/sites/#{application}"
-set :rails_env, "production"
+set :rails_env, "staging"
 set :branch, "master"
-# set :rails_env, "staging"
-# set :branch, "staging"
 set :unicorn_pid, "#{deploy_to}/current/tmp/pids/unicorn.pid"
 
-# Server innercircle(production)
-role :app, "108.166.93.213:2012"
-role :web, "108.166.93.213:2012"
-role :db,  "108.166.93.213:2012", :primary => true
+role :app, "108.166.89.157:2012"
+role :web, "108.166.89.157:2012"
+role :db,  "108.166.89.157:2012", :primary => true
 
