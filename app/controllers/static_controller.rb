53da6a7d460b31046203f02375c274c6c6c1583e
Kenny Chan <kenny.r.chan@gmail.com>
2013-08-19 09:43:22 -0400

    fix robots.txt location, and add a blocked list, with "talent" in it to start

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,8 +3,13 @@ class StaticController < ApplicationController
   def for_companies
   end
 
-  def robots        
-    robots = File.read(Rails.root + "config/robots.#{Rails.env}.txt")
+  def robots
+    disallowed_subdomains = ['talent']
+    if disallowed_subdomains.include? request.subdomain
+      robots = File.read(Rails.root + "config/robots.blocked.txt")
+    else
+      robots = File.read(Rails.root + "config/robots.#{Rails.env}.txt")
+    end
     render :text => robots, :layout => false, :content_type => "text/plain"
   end
 end
