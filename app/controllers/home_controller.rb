9611752dec920f6a5798d33a89735eb28c2a5da6
Elliott Golden <studio@simplecircle.net>
2013-10-17 18:01:14 -0400

    homepage layout WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
-  layout 'marketing'
-  def index
 
+  layout 'marketing'
+
+  def index
+  	if current_user
+  	else
+      @companies = Company.where(:show_in_index=>true).order('last_published_posts_at DESC')
+    end
   end
 end
