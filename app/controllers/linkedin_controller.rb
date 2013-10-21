be1c87323bceffbf6b8c1a104e4702f5dab89695
Elliott Golden <studio@simplecircle.net>
2013-10-21 17:27:14 -0400

    company_connections hash built from linkedin connections

--- app/controllers/linkedin_controller.rb
+++ app/controllers/linkedin_controller.rb
@@ -9,11 +9,36 @@ class LinkedinController < ApplicationController
   def create
   	if auth = request.env["omniauth.auth"]
   		# raise auth.to_yaml
-  		logger.info "+++++++++++++++++++++++"
-  		logger.info auth["credentials"].token
+  		company_connections = {}
+  		company_blacklist = ["freelance"]
   		access_token = auth["credentials"].token
-        response = HTTParty.get("https://api.linkedin.com/v1/people/~/connections:(id,first-name,last-name,positions:(title,is_current,company:(id,name)))", :query=>{oauth2_access_token:access_token, format:"json"})
-  		logger.info response.body
+        response = HTTParty.get("https://api.linkedin.com/v1/people/~/connections:(id,first-name,last-name,public-profile-url,picture-url,positions:(title,is_current,company:(id,name)))", :query=>{oauth2_access_token:access_token, format:"json"})
+  		response = JSON.parse(response.body)
+  		 # logger.info response
+
+  		response['values'].each do |connection|
+  	 	  built_connection = {}
+  	 	  built_connection['id'] = connection['id']
+  	 	  built_connection['first_name'] = connection['firstName']
+  	 	  built_connection['last_name'] = connection['lastName']
+  	 	  built_connection['public_profile_url'] = connection['publicProfileUrl']
+  	 	  built_connection['picture_url'] = connection['pictureUrl']
+
+  		  if connection['positions']
+  		  	if connection['positions']['values']
+  		  	  connection['positions']['values'].each do |position|
+  		  	 	if position['company']['id']
+  		  	 	  (company_connections[position['company']['id'].to_s.downcase] ||= []) << built_connection
+  		  	 	else 
+  		  	 	  unless company_blacklist.include?(position['company']['name'].to_s.downcase)
+    		  	 	(company_connections[position['company']['name'].to_s.downcase] ||= []) << built_connection
+    		  	  end
+  		  	    end
+  		  	  end
+  		    end
+  		  end
+  		end
+  		logger.info company_connections
   		# redirect_to(user_path(current_user))
   	end
   end
