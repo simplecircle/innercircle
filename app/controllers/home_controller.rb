89d423c7a4d8549087a59569719a01e9db4c67b7
Elliott Golden <studio@simplecircle.net>
2013-10-26 15:28:00 -0400

    feed autoscroll wired up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,14 @@
+require 'will_paginate/array'
+
 class HomeController < ApplicationController
 
   def index
   	if current_user
-  	  @posts = Post.following_stream(current_user, 5000)
-      render "user_index"
+  	  @posts = Post.following_stream(current_user, 5000).paginate(:page => params[:page], per_page:15)
+  	  respond_to do |format|
+        format.html {render("user_index")}
+        format.js {render("posts/published.js.erb")}
+      end
   	else
       @companies = Company.where(:show_in_index=>true).order('last_published_posts_at DESC')
     end
