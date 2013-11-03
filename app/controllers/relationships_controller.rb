d9e39d0a9003337ff7c26ebca716726272ada63f
Elliott Golden <studio@simplecircle.net>
2013-11-03 14:29:19 -0500

    add follow button size option

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,6 +5,7 @@ class RelationshipsController < ApplicationController
   def create
     if current_user
      if current_user.follow!(@company)
+      @size = relationship_params[:size]
       # @follower_count = @company.followers.count
      end
     else
@@ -15,6 +16,7 @@ class RelationshipsController < ApplicationController
   def destroy
     if current_user and current_user.following?(@company)
       current_user.unfollow!(@company)
+      @size = relationship_params[:size]
       # @follower_count = @company.followers.count
     else
       head 403
@@ -23,8 +25,11 @@ class RelationshipsController < ApplicationController
 
   private
 
+  def relationship_params
+    params.permit!
+  end
   def find_resource
-    @company = Company.find(params[:company_id])
+    @company = Company.find(relationship_params[:company_id])
   end
 end
 
